c020489bb097f85d0ee2d93bcd9c823bcc1ede3f
eramoss <edulramos@outlook.com>
2023-10-22 18:31:25 -0300

    update main

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,12 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import qualified CreditCardEx (toDigit)
 import qualified Control.Applicative as MyLib
+import Data.Type.Equality (TestEquality(testEquality))
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  let x = CreditCardEx.toDigit 95240687
+  print x
+
