0fc211fe3623eb8913711a9d0750e466f40148a2
eramoss <edulramos@outlook.com>
2023-10-22 18:52:33 -0300

    create reverse to digit

--- src/CreditCardEx.hs
+++ src/CreditCardEx.hs
@@ -1,7 +1,11 @@
-module CreditCardEx (toDigit) where
+module CreditCardEx (toDigit, reverseToDigit) where
 import Data.Char (ord, digitToInt)
 import Data.Fixed (div', mod')
 
 toDigit:: Integer -> [Integer]
 toDigit 0 = []
 toDigit n = toDigit (n `div` 10) ++ [n `mod` 10]
+
+reverseToDigit :: Integer -> [Integer]
+reverseToDigit 0 = []
+reverseToDigit n = reverse( toDigit n)
