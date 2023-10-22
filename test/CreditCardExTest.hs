e707b5deef6fc571c584b3971898ecb0e085fda7
eramoss <edulramos@outlook.com>
2023-10-22 18:52:44 -0300

    create test for reverse digit

--- test/CreditCardExTest.hs
+++ test/CreditCardExTest.hs
@@ -3,14 +3,16 @@ import Test.Framework
 import Test.Framework.Providers.HUnit
 import Data.Monoid
 import Control.Monad
-import qualified CreditCardEx (toDigit)
+import qualified CreditCardEx (toDigit, reverseToDigit)
 
 toDigitTest :: Assertion
 toDigitTest = assertEqual "toDigit test with zero" [5,3,4] (CreditCardEx.toDigit 0534)
 
-
+reverseToDigitTest :: Assertion
+reverseToDigitTest = assertEqual "reverseToDigit default test" [5,3,4] (CreditCardEx.reverseToDigit 435)
 
 main :: IO ()
 main = defaultMainWithOpts
-       [testCase "to digit" toDigitTest]
+       [testCase "to digit" toDigitTest
+       ,testCase "reverse to digit" reverseToDigitTest]
        mempty
